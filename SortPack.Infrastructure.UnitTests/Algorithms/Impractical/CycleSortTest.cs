570fceb1380fd526f6c3eb49f613d697c68fa81d
Paul Keller <contact.keller.paul@gmail.com>
2024-06-10 20:03:36 +0200

    added cycle sort

---
+++ SortPack.Infrastructure.UnitTests/Algorithms/Impractical/CycleSortTest.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using FluentAssertions.AssertMultiple;
+using SortPack.Infrastructure.Impractical;
+
+namespace SortPack.Infrastructure.UnitTests.Algorithms.Impractical;
+
+public class CycleSortTest : SortAlgorithmTestBase<CycleSort>
+{
+    [Test]
+    public override void SortInPlace_Uneven_WhenCalled_SortsCollection()
+    {
+        // Arrange
+        List<int> collection = [3, 2, 1];
+
+        // Act
+        Sut?.SortInPlace(collection);
+
+        // Assert
+        AssertMultiple.Multiple(() =>
+        {
+            collection.Should().BeEquivalentTo(new List<int> { 1, 2, 3 });
+            StatisticCounter?.ReadOperations.Should().Be(11);
+            StatisticCounter?.WriteOperations.Should().Be(2);
+            StatisticCounter?.CompareOperations.Should().Be(12);
+
+        });
+    }
+
+    [Test]
+    public override void SortInPlace_Even_WhenCalled_SortsCollection()
+    {
+        // Arrange
+        List<int> collection = [3, 2, 1, 0];
+
+        // Act
+        Sut?.SortInPlace(collection);
+
+        // Assert
+        AssertMultiple.Multiple(() =>
+        {
+            collection.Should().BeEquivalentTo(new List<int> { 0, 1, 2, 3 });
+            StatisticCounter?.ReadOperations.Should().Be(22);
+            StatisticCounter?.WriteOperations.Should().Be(4);
+            StatisticCounter?.CompareOperations.Should().Be(26);
+
+        });
+    }
+
+    [Test]
+    public override void SortInPlace_EmptyList_DoNothing()
+    {
+        // Arrange
+        List<int> collection = [];
+
+        // Act
+        Sut?.SortInPlace(collection);
+
+        // Assert
+        AssertMultiple.Multiple(() =>
+        {
+            StatisticCounter?.ReadOperations.Should().Be(0);
+            StatisticCounter?.WriteOperations.Should().Be(0);
+            StatisticCounter?.CompareOperations.Should().Be(0);
+        });
+    }
+}
---
+++ SortPack.Infrastructure/Impractical/CycleSort.cs
@@ -0,0 +1,74 @@
+﻿using SortPack.Domain.Abstractions;
+using SortPack.Domain.Interfaces;
+
+namespace SortPack.Infrastructure.Impractical;
+
+public class CycleSort : SortAlgorithm
+{
+    public CycleSort()
+    {
+    }
+    public CycleSort(IStatisticCounter statisticCounter) : base(statisticCounter)
+    {
+    }
+    public override IList<T> SortInPlace<T>(IList<T> collection)
+    {
+        int n = collection.Count;
+        for (int cycleStart = 0; cycleStart < n - 1; cycleStart++)
+        {
+            T item = collection[cycleStart];
+            StatisticCounter?.IncrementReadOperations();
+            int pos = cycleStart;
+            for (int i = cycleStart + 1; i < n; i++)
+            {
+                if (LessThan(collection[i], item))
+                {
+                    pos++;
+                }
+            }
+            StatisticCounter?.IncrementReadOperations((ulong)(n - cycleStart - 1));
+            StatisticCounter?.IncrementCompareOperations((ulong)(n - cycleStart - 1));
+            if (pos == cycleStart)
+            {
+                continue;
+            }
+            StatisticCounter?.IncrementReadOperations();
+            while (Equal(collection[pos], item))
+            {
+                pos++;
+                StatisticCounter?.IncrementReadOperations();
+            }
+
+            if (pos != cycleStart)
+            {
+                (item, collection[pos]) = (collection[pos], item);
+                StatisticCounter?.IncrementWriteOperations();
+                StatisticCounter?.IncrementReadOperations();
+            }
+
+            while (pos != cycleStart)
+            {
+                pos = cycleStart;
+                for (int i = cycleStart + 1; i < n; i++)
+                {
+                    if (LessThan(collection[i], item))
+                    {
+                        pos++;
+                    }
+                }
+                StatisticCounter?.IncrementReadOperations((ulong)(n - cycleStart - 1));
+                StatisticCounter?.IncrementCompareOperations((ulong)(n - cycleStart - 1));
+                StatisticCounter?.IncrementReadOperations();
+                while (Equal(collection[pos], item))
+                {
+                    pos++;
+                    StatisticCounter?.IncrementReadOperations();
+                }
+                (item, collection[pos]) = (collection[pos], item);
+                StatisticCounter?.IncrementWriteOperations();
+                StatisticCounter?.IncrementReadOperations();
+            }
+        }
+        return collection;
+    }
+}
